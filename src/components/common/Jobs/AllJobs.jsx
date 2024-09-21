838a9c183e5cfa2b9d71da7ec96b8f0951011fe7
Bhushan-20 <bhushan72845@gmail.com>
2024-09-21 11:09:04 +0530

    changed loader

--- src/components/common/Jobs/AllJobs.jsx
+++ src/components/common/Jobs/AllJobs.jsx
@@ -135,7 +135,14 @@ const AllJobs = () => {
             {/* Display loader or jobs table */}
             {loading ? (
                 <div className="flex justify-center items-center">
-                    <div className="spinner"></div> {/* Spinner instead of text */}
+                    <div id='load'><div>G</div>
+                        <div>N</div>
+                        <div>I</div>
+                        <div>D</div>
+                        <div>A</div>
+                        <div>O</div>
+                        <div>L</div>
+                    </div>
                 </div>
             ) : filteredJobs.length > 0 ? (
                 <AllJobsTable jobs={filteredJobs} setJobs={setJobs} />
