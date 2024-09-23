5fddf86d6fdc7951a675ae822e34b1ee663908a8
Bhushan-20 <bhushan72845@gmail.com>
2024-09-23 11:22:26 +0530

    changes made in sendMail function

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -15,7 +15,7 @@ const mailSender = async (email, title, body) => {
             })
 
             let info = await transporter.sendMail({
-                from: 'Jobify || From Bhushan Patil ',
+                from:'"Jobify" <your-email@example.com>',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
